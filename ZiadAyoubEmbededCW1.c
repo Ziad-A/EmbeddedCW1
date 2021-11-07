3439d764a51baef6fb033e224ddcbb94f22a4588
BuildTools <unconfigured@null.spigotmc.org>
2021-11-07 15:55:23 +0200

    Second commit

--- ZiadAyoubEmbededCW1.c
+++ ZiadAyoubEmbededCW1.c
@@ -4,8 +4,8 @@
   Coursework 1 Code
   Embedded System Design and Development
   2021 - 2022
-  Version: V1
-  
+  Version: V2
+
 */
 
 
@@ -25,11 +25,14 @@ void main() {
 
 
   ANSELA = 0b11111111;
-  ANSELD = 0;
   ANSELC = 0;                     // Configure PORTC pins as digital
+  ANSELD = 0;
+  ANSELE = 0;
+
 
   TRISA=0b11111111;
   TRISD=0b11111111;
+  TRISE=0;
 
   LDRButton = 0;
 
@@ -37,8 +40,8 @@ void main() {
 
   LDR = ADC_Read(1);
   VarRis = ADC_Read(0);
-  
-  
+
+
   Delay_ms(1000);
   if (Button(&PORTD, 7, 10, 1)){LDRButton = 1;}
   if (Button(&PORTD, 6, 10, 1)){LDRButton = 0;}
@@ -50,39 +53,53 @@ void main() {
      UART1_Write(10);
   }
 
-  else if (LDRButton == 1)
-  {
-      UART1_Write_Text("LDR Readings are ON!");
-
-      if (LDR>=4000){
-        UART1_Write_Text("The room is very bright");
-        UART1_Write(13);
-        UART1_Write(10);
-      }
-
-      else if (LDR>=2200){
-        UART1_Write_Text("The room is bright");
-        UART1_Write(13);
-        UART1_Write(10);
-      }
-	  
-      else if (LDR>=1200){
-        UART1_Write_Text("The room is dim");
-        UART1_Write(13);
-        UART1_Write(10);
-      }
-
-      else if (LDR>=0){
-        UART1_Write_Text("The room is dark");
-        UART1_Write(13);
-        UART1_Write(10);
-      }
-    }
+  else if (LDRButton == 1){
+      UART1_Write_Text("LDR Readings are ON! ");
+      UART1_Write_Text("The room is: ");
   
-  UART1_Write_Text("Variable Resistor Level: ");
-  UART1_Write(VarRis);
+      if (LDR >= 4000){
+        UART1_Write_Text("very bright!");
+      }
+
+      else if (LDR >= 2200){
+        UART1_Write_Text("bright!");
+      }
+      
+      else if (LDR >= 1200){
+        UART1_Write_Text("dim!");
+      }
+      
+      else{
+        UART1_Write_Text("dark!");
+      }
+      
+      UART1_Write(13);
+      UART1_Write(10);
+    }
+
+
+  UART1_Write_Text("The light level due to the variable resistor is: ");
+
+  if (VarRis >= 2600){
+    UART1_Write_Text("bright!");
+    PORTE = 0b00000111;
+  }
+
+  else if (VarRis >= 1900){
+    UART1_Write_Text("dim!");
+    PORTE = 0b00000011;
+  }
+  
+  else{
+    UART1_Write_Text("low/OFF!");
+    PORTE = 0b00000001;
+  }
+  
+
   UART1_Write(13);
   UART1_Write(10);
-  
+  UART1_Write_Text(" ");
+  UART1_Write(13);
+  UART1_Write(10);
   }
 }
