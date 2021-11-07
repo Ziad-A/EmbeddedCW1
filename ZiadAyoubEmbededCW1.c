1d6774152120683682b458568495ca1e6e33196a
BuildTools <unconfigured@null.spigotmc.org>
2021-11-07 15:56:22 +0200

    Third commit

--- ZiadAyoubEmbededCW1.c
+++ ZiadAyoubEmbededCW1.c
@@ -4,55 +4,68 @@
   Coursework 1 Code
   Embedded System Design and Development
   2021 - 2022
-  Version: V2
+  Version: V4
 
 */
 
 
+// Initialization of variables
 char uart_rd;
-int LDR;
-int VarRis;
-int LDRButton;
+int LDR;						  // LDR reading
+int VarRis;						  // Potentiometer reading 
+int LDRButton;					  // Button staus wether 1 (readings toggeled on) or 0 (readings toggeled off)
+
 
 void main() {
 
   UART1_Init(9600);               // Initialize UART module at 9600 bps
   Delay_ms(100);                  // Wait for UART module to stabilize
 
-  UART1_Write_Text("Start");
+  UART1_Write_Text("Start");	  // Writes on the UART Terminal the word "Start" for visual confirmation of succesful UART Initialization
   UART1_Write(13);
   UART1_Write(10);
 
 
-  ANSELA = 0b11111111;
-  ANSELC = 0;                     // Configure PORTC pins as digital
-  ANSELD = 0;
-  ANSELE = 0;
+  ANSELA = 0b11111111;			  // Configure PORT A pins as analouge
+  ANSELC = 0;                     // Configure PORT C pins as digital
+  ANSELD = 0;					  // Configure PORT D pins as digital
+  ANSELE = 0;					  // Configure PORT E pins as digital
 
 
-  TRISA=0b11111111;
-  TRISD=0b11111111;
-  TRISE=0;
+  TRISA=0b11111111;			   	  // Configure PORT A pins as input
+  TRISD=0b11111111;				  // Configure PORT D pins as input
+  TRISE=0;						  // Configure PORT E pins as output
 
-  LDRButton = 0;
+  LDRButton = 0;				  // Set LDRButton to 0 which turns readings off by defult,
+								  // setting it to 1 would turn readings on.
+  
 
-  while(1){
+  while(1){						  // Goes into infinite loop
 
-  LDR = ADC_Read(1);
-  VarRis = ADC_Read(0);
+  LDR = ADC_Read(1);			  // Reads LDR value through the analoug to digital converter through pin RA1
+  VarRis = ADC_Read(0);			  // Reads Potentiometer value through the analoug to digital converter through pin RA0
 
 
-  Delay_ms(1000);
+  Delay_ms(1000);				  // Time delay of 1 sec between readings 
+  
+  
+  // Checks if buttons RD7 or RD6 are pressed
+  // If RD7 is pressed, readings from the LDR are taken
+  // If RD6 is pressed, readings for the LDR are switched off 
   if (Button(&PORTD, 7, 10, 1)){LDRButton = 1;}
   if (Button(&PORTD, 6, 10, 1)){LDRButton = 0;}
 
 
+  // If readings for the LDR are switched off, outputs visual message that it is off
   if (LDRButton == 0){
      UART1_Write_Text("LDR Readings are OFF!");
      UART1_Write(13);
      UART1_Write(10);
   }
 
+
+  // If readings for the LDR are switched on, outputs visual message that it is on
+  // It then takes the reading of the LDR and comapres it to the 4 preassigned ranges to determin the state of the room's brightness level
   else if (LDRButton == 1){
       UART1_Write_Text("LDR Readings are ON! ");
       UART1_Write_Text("The room is: ");
@@ -78,6 +91,9 @@ void main() {
     }
 
 
+  // Takes the reading from the potentiometer and compares it to the 3 preassigned value
+  // Depending on the potentiometer level, 1, 2, or 3, lighs on PORT E turn on
+  // A visual message on the terminal is also displayed  
   UART1_Write_Text("The light level due to the variable resistor is: ");
 
   if (VarRis >= 2600){
@@ -96,6 +112,7 @@ void main() {
   }
   
 
+  // Creates an empty line to seprate between readings
   UART1_Write(13);
   UART1_Write(10);
   UART1_Write_Text(" ");
